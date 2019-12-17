e4c2e6886fd196c2e8f2cc65e637977467ebaef8
Turic Ioana <turicioana@gmail.com>
2019-12-17 12:51:29 +0200

    Added Angular Client for application

--- fundraisingapp/src/main/java/fundraisingapp/Auth/Service/JWTRequestFilter.java
+++ fundraisingapp/src/main/java/fundraisingapp/Auth/Service/JWTRequestFilter.java
@@ -26,7 +26,6 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         final  String requestHeader =  httpServletRequest.getHeader("Authorization");
-        System.out.println("BBB " + requestHeader);
         String email = null;
         String jwtToken = null;
         if(requestHeader != null && requestHeader.startsWith("Bearer ")){
